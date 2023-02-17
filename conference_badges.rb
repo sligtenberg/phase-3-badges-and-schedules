98c9abe48865c62269e617ed26f87648ffa7e032
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-17 00:37:47 -0500

    commit tree 116520fabd5240beb17307981d09fa5058eba23a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array_of_names
+    array_of_names.map { |name| badge_maker(name) }
+end
+
+def assign_rooms array_of_speakers
+    array_of_speakers.map.with_index(1) { |speaker, room| "Hello, #{speaker}! You'll be assigned to room #{room}!" }
+end
+
+def printer array_of_speakers
+    batch_badge_creator(array_of_speakers).each { |badge| puts badge }
+    assign_rooms(array_of_speakers).each { |assignment| puts assignment }
+end
